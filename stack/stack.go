45d06654d648c5e38af7067dc94b7b8d49f87559
nmezhenskyi <nikita.mezhenskyi@gmail.com>
2024-05-06 22:11:56 -0400

    feat(stack): stack implementation

---
+++ stack/stack.go
@@ -0,0 +1,52 @@
+/*
+	The MIT License (MIT)
+
+	Copyright (c) 2024 Nikita Mezhenskyi
+
+	Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+	and associated documentation files (the "Software"), to deal in the Software without restriction,
+	including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+	and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+	subject to the following conditions:
+
+	The above copyright notice and this permission notice shall be included in all copies or
+	substantial portions of the Software.
+
+	THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+	INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+	NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+	DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+	OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+// Package stack implements a Stack data structure.
+package stack
+
+type Stack[T any] struct {
+	stack []T
+	size  int
+}
+
+func NewStack[T any]() *Stack[T] {
+	return &Stack[T]{stack: []T{}}
+}
+
+func (s *Stack[T]) Push(val T) {
+	s.stack = append(s.stack, val)
+	s.size++
+}
+
+func (s *Stack[T]) Pop() T {
+	val := s.stack[s.size-1]
+	s.stack = s.stack[:s.size-1]
+	s.size--
+	return val
+}
+
+func (s *Stack[T]) Top() T {
+	return s.stack[s.size-1]
+}
+
+func (s *Stack[T]) IsEmpty() bool {
+	return s.size == 0
+}
